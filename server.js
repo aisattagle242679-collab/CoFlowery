24b512be9f2be16da4848f7b3272a883a6248a97
aisattagle242679-collab <aisat.tagle242679@gmail.com>
2026-06-06 23:11:27 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { Pool } = require('pg');
 
 const app = express();
@@ -7,6 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// Serve static files (HTML, CSS, JS, assets) from the project root
+app.use(express.static(path.join(__dirname)));
 
 // Connect to PostgreSQL cloud database via environment variable
 const pool = new Pool({
